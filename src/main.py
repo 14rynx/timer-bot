9771e9d1fee130581da7ecd67efa7c0fd7a34fb9
larynx <larynx.austrene@gmail.com>
2025-03-21 10:00:42 +0100

    Made action text go to all users.

--- src/main.py
+++ src/main.py
@@ -245,14 +245,13 @@ async def action(ctx, *action_text):
 
     user_count = 0
     for user in User.select():
-        if user.characters.exists():
-            try:
-                channel = await get_channel(user, bot)
-                await channel.send(action_text_concatenated)
-            except discord.errors.Forbidden:
-                await ctx.send(f"Could not reach user {user}.")
-                logger.info(f"Could not reach user {user}.")
-            user_count += 1
+        try:
+            channel = await get_channel(user, bot)
+            await channel.send(action_text_concatenated)
+        except discord.errors.Forbidden:
+            await ctx.send(f"Could not reach user {user}.")
+            logger.info(f"Could not reach user {user}.")
+        user_count += 1
 
     await ctx.send(f"Sent action text to {user_count} users. The message looks like the following:")
     await ctx.send(action_text_concatenated)
