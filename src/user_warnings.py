1ac5b320283a22b4a5a41e06df010676a2b2ebf2
larynx <larynx.austrene@gmail.com>
2025-03-27 11:13:52 +0100

    Switched to multiline strings to fix whitespace

--- src/user_warnings.py
+++ src/user_warnings.py
@@ -32,20 +32,19 @@ async def send_esi_permission_warning(character: Character, channel, preston: Pr
             character_id=character.character_id
         ).get("name")
 
-        warning_text = f"""
-        ### WARNING
-        The following character does not have permissions to fetch data from ESI:
-        {character_name}
-        - If you to not intend to use this character anymore, remove him with `!revoke {character_name}`.
-        - Otherwise re-authenticate with `!auth`.
-        """
+        warning_text = (
+            "### WARNING\n"
+            f"The following character does not have permissions to fetch data from ESI: {character_name}\n"
+            "- If you to not intend to use this character anymore, remove him with `!revoke {character_name}`.\n"
+            "- Otherwise re-authenticate with `!auth`."
+        )
     except (ValueError, KeyError, JSONDecodeError):
-        warning_text = f"""
-        ### WARNING
-        One of your characters does not have permissions to fetch data from ESI.
-        - If you to not intend to use this bot anymore, remove your characters with `!revoke`. 
-        - Otherwise re-authenticate with `!auth`.
-        """
+        warning_text = (
+            "### WARNING\n"
+            "One of your characters does not have permissions to fetch data from ESI.\n"
+            "- If you to not intend to use this bot anymore, remove your characters with `!revoke`.\n"
+            "- Otherwise re-authenticate with `!auth`."
+        )
 
     await send_warning(character.user, channel, warning_text, log_text=f"esi_permission_warning for {character}",
                        **kwargs)
@@ -56,14 +55,13 @@ async def send_structure_permission_warning(character: Character, channel, authe
 
     character_name = authed_preston.whoami()["CharacterName"]
 
-    warning_text = f"""
-    ### WARNING
-    The following character does not have permissions to see structure info:
-    {character_name}
-    - If you to not intend to use this character, remove him with `!revoke {character_name}`. 
-    - Otherwise, fix your corp permissions in-game. To do that, go to "Corporation" -> "Administration" -> \
-    "Role Management" -> "Station Services" and add the "Station Manager" role, then check them with `!info`.
-    """
+    warning_text = (
+        "### WARNING\n"
+        f"The following character does not have permissions to see structure info: {character_name}\n"
+        f"- If you to not intend to use this character, remove him with `!revoke {character_name}`.\n"
+        "- Otherwise, fix your corp permissions in-game. To do that, go to \"Corporation\" -> \"Administration\" -> "
+        "\"Role Management\" -> \"Station Services\" and add the \"Station Manager\" role, then check them with `!info`."
+    )
 
     await send_warning(character.user, channel, warning_text, log_text=f"structure_permission_warning for {character}",
                        **kwargs)
@@ -73,14 +71,14 @@ async def send_structure_corp_warning(character: Character, channel, authed_pres
     """Send a warning to users who have changed corp."""
     character_name = authed_preston.whoami()["CharacterName"]
 
-    warning_text = f"""
-    ### WARNING
-    The following character has changed corporation and can thus no longer see structure info of the old corporation:
-    {character_name}.
-    - If you to not intend to use this character, remove him with `!revoke {character_name}`.
-    - If you want to use this character again with the new corporation, `!auth` again.
-    - If you want to use this character again with the old corporation, re-join the old corporation in-game.
-    """
+    warning_text = (
+        "### WARNING\n"
+        "The following character has changed corporation and can thus no "
+        f"longer see structure info of the old corporation: {character_name}.\n"
+        "- If you to not intend to use this character, remove him with `!revoke {character_name}`.\n"
+        "- If you want to use this character again with the new corporation, `!auth` again.\n"
+        "- If you want to use this character again with the old corporation, re-join the old corporation in-game."
+    )
 
     await send_warning(character.user, channel, warning_text, log_text=f"structure_corp_warning for {character}",
                        **kwargs)
@@ -90,26 +88,24 @@ async def send_structure_other_warning(character: Character, channel, authed_pre
                                        **kwargs):
     character_name = authed_preston.whoami()["CharacterName"]
 
-    warning_text = f"""
-    ### WARNING
-    The following character can not see structure info:
-    {character_name}
-    This is due to the following error:
-    {error_value}
-    There are no specific instructions to fix this error, so you have to try for yourself.
-    - In case you no longer need structure pings, you can remove the character with `!revoke {character_name}`.
-    - Otherwise you can check if your permissions are correct with `!info`.
-    """
+    warning_text = (
+        "### WARNING\n"
+        f"The following character does not have permissions to see structure info: {character_name}\n"
+        f"This is due to the following error: {error_value}\n"
+        "There are no specific instructions to fix this error, so you have to try for yourself.\n"
+        "- In case you no longer need structure pings, you can remove the character with `!revoke {character_name}`.\n"
+        "- Otherwise you can check if your permissions are correct with `!info`."
+    )
 
     await send_warning(character.user, channel, warning_text, log_text=f"structure_other_warning for {character}",
                        **kwargs)
 
 
 async def send_channel_warning(user, channel, **kwargs):
-    warning_text = f"""
-    ### WARNING
-    The channel you are using for timer-bot callbacks is a private channel which the bot might eventually no longer \
-    be able to reach. Please use a channel in a server as your callback location.
-    """
+    warning_text = (
+        "### WARNING\n"
+        "The channel you are using for timer-bot callbacks is a private channel which the bot might eventually no longer "
+        "be able to reach. Please use a channel in a server as your callback location."
+    )
 
     await send_warning(user, channel, warning_text, log_text=f"channel_warning for {user}", **kwargs)
