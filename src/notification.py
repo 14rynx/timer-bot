9ec70662be2d9cc510a8026ba49a1c2de8128ec2
larynx <larynx.austrene@gmail.com>
2025-06-08 14:44:01 +0200

    Added poco notifications

--- src/notification.py
+++ src/notification.py
@@ -64,6 +64,44 @@ def structure_notification_text(notification: dict, authed_preston: Preston) -> 
         case _:
             return ""
 
+def get_poco_name(notification: dict, preston: Preston) -> str:
+    """returns a structure id from the notification or none if no structure_id can be found"""
+    planet_id = None
+    for line in notification.get("text").split("\n"):
+        if "planetID:" in line:
+            planet_id = line.split(" ")[2]
+
+    if planet_id is not None:
+        return preston.get_op("get_universe_plantes_planet_id", planet_id=planet_id).get("name")
+    return "Unknown Poco"
+
+
+def poco_notification_text(notification: dict, preston: Preston) -> str:
+    """Returns a human-readable message of a structure notification"""
+
+    match notification.get('type'):
+        case "OrbitalAttacked":
+            # Parse attacker info
+            character_id = get_attacker_character_id(notification)
+            if character_id is not None:
+                character_name = preston.get_op(
+                    'get_characters_character_id',
+                    character_id=str(character_id)
+                ).get("name", "Unknown")
+                attribution = f" by [{character_name}](https://zkillboard.com/character/{character_id}/)"
+            else:
+                attribution = ""
+            return f"@everyone {get_poco_name(notification, preston)} is under attack{attribution}!\n"
+        case "OrbitalReinforced":
+            return f"@everyone {get_poco_name(notification, preston)} has ben reinforced!\n"
+        case _:
+            return ""
+
+
+def is_poco_notification(notification: dict) -> bool:
+    """returns true if a notification is about a structure"""
+    # All structure notifications start with Structure... so we can use that
+    return "Orbital" in notification.get('type')
 
 def is_structure_notification(notification: dict) -> bool:
     """returns true if a notification is about a structure"""
@@ -75,19 +113,33 @@ async def send_notification_message(notification, user_channel, authed_preston, 
     """For a notification from ESI take action and inform a user if required"""
     notification_id = notification.get("notification_id")
 
-    if not is_structure_notification(notification):
-        return
-
     notif, created = Notification.get_or_create(notification_id=notification_id)
 
-    if not notif.sent:
-        try:
-            if len(message := structure_notification_text(notification, authed_preston)) > 0:
-                await user_channel.send(message)
-                logger.debug(f"Sent notification to {identifier}")
+    if is_structure_notification(notification):
+        if not notif.sent:
+            try:
+                if len(message := structure_notification_text(notification, authed_preston)) > 0:
+                    await user_channel.send(message)
+                    logger.debug(f"Sent structure notification to {identifier}")
 
-        except Exception as e:
-            logger.warning(f"Could not send notification to {identifier}: {e}")
-        else:
+            except Exception as e:
+                logger.warning(f"Could not send structure notification to {identifier}: {e}")
+            else:
+                notif.sent = True
+                notif.save()
+    elif is_poco_notification(notification):
+        if not notif.sent:
+            try:
+                if len(message := poco_notification_text(notification, authed_preston)) > 0:
+                    # await user_channel.send(message) # Not used for now
+                    logger.debug(f"Sent poco notification to {identifier}")
+
+            except Exception as e:
+                logger.warning(f"Could not send poco notification to {identifier}: {e}")
+            else:
+                notif.sent = True
+                notif.save()
+    else:
+        if not notif.sent:
             notif.sent = True
             notif.save()
